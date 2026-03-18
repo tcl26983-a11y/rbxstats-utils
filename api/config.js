4e59d19c9089080a64e79f07f0356ef368d8e508
tcl26983-a11y <tcl26983@gmail.com>
2026-03-18 08:35:57 +0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,4 +1,8 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   res.status(200).json({
     token: process.env.TELEGRAM_TOKEN,
     chat_id: process.env.CHAT_ID
